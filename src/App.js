048f6236ed447270871bb049df772931870ac20f
jasmeet1233 <jazzratra@gmail.com>
2021-07-26 00:03:18 +0530

    React

--- src/App.js
+++ src/App.js
@@ -1,38 +1,48 @@
-import React from "react";
-import Header from "./Header";
-import Content from "./Content";
-import Total from "./Total.js";
+import React, { useState } from "react";
+import Button from "./Button.js";
+import Display from "./Display.js";
+import History from "./History.js";
+import Statistics from "./Statistics.js";
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const anecdotes = [
+    "If it hurts, do it more often",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blod tests when dianosing patients",
+  ];
 
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
+  const maxVotesNumber = Math.max(...votes);
+  const mostVotesIndex = votes.indexOf(maxVotesNumber);
+
+  const clickHandler = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length));
+  };
+
+  const voteHandler = () => {
+    const updatedVotes = [...votes];
+    updatedVotes[selected] += 1;
+    setVotes(updatedVotes);
+  };
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts = {course.parts} />
-      <Total
-        exercises = {course.parts}
-      />
+      <h1>Anectode of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <Button clickHandler={clickHandler} text="Next Anectode" />
+      <Button clickHandler={voteHandler} text="Vote" />
+      <h1>Anectode with most Votes</h1>
+      <p>
+        {anecdotes[mostVotesIndex]} has {maxVotesNumber} votes
+      </p>
     </div>
-    )
+  );
 };
 
 export default App;
-
